dd6e93c9f1f877c67e1d9b951dd7324b423e2ab5
Charith Bhat <charsept04@gmail.com>
2021-01-06 13:36:15 +0530

    local notification attempt 2

--- lib/widgets/add_todo_dialog.dart
+++ lib/widgets/add_todo_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_application/models/todo.dart';
 import 'package:todo_application/provider/todo_list.dart';
@@ -13,18 +14,67 @@ class _AddTodoDialogState extends State<AddTodoDialog> {
   final _formkey = GlobalKey<FormState>();
   String title = '';
   String description = '';
+  FlutterLocalNotificationsPlugin fltrNotification;
+  String _selectedParam;
+  String task;
+  int val;
+
+  @override
+  void initState() {
+    super.initState();
+    var androidInitilize = new AndroidInitializationSettings('app_icon');
+    var iOSinitilize = new IOSInitializationSettings();
+    var initilizationsSettings = new InitializationSettings(
+        android: androidInitilize, iOS: iOSinitilize);
+    fltrNotification = new FlutterLocalNotificationsPlugin();
+    fltrNotification.initialize(initilizationsSettings,
+        onSelectNotification: notificationSelected);
+  }
+
+  Future _showNotification() async {
+    var androidDetails = new AndroidNotificationDetails(
+        "Channel ID", "Desi programmer", "This is my channel",
+        importance: Importance.max);
+    var iSODetails = new IOSNotificationDetails();
+    var generalNotificationDetails =
+        new NotificationDetails(android: androidDetails, iOS: iSODetails);
+
+    // await fltrNotification.show(
+    //     0, "Task", "You created a Task", generalNotificationDetails, payload: "Task");
+    var scheduledTime;
+    if (_selectedParam == "Hour") {
+      scheduledTime = DateTime.now().add(Duration(hours: val));
+    } else if (_selectedParam == "Minute") {
+      scheduledTime = DateTime.now().add(Duration(minutes: val));
+    } else {
+      scheduledTime = DateTime.now().add(Duration(seconds: val));
+    }
+
+    fltrNotification.schedule(
+        1, "Times Uppp", task, scheduledTime, generalNotificationDetails);
+  }
+
+  Future notificationSelected(String payload) async {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: Text("Notification Clicked $payload"),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     //var height = MediaQuery.of(context).size.height;
     //var width = MediaQuery.of(context).size.width;
     return AlertDialog(
+      insetPadding: EdgeInsets.symmetric(horizontal: 0),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(10)),
       ),
       content: Container(
-        // height: height / 3,
-        // width: width / 2,
+        //height: height,
+        //width: width,
         child: Form(
           key: _formkey,
           child: Column(
@@ -44,6 +94,86 @@ class _AddTodoDialogState extends State<AddTodoDialog> {
                   });
                 },
                 onSavedTodo: addTodo,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextField(
+                  decoration: InputDecoration(border: OutlineInputBorder()),
+                  onChanged: (_val) {
+                    task = _val;
+                  },
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  DropdownButton(
+                    value: _selectedParam,
+                    items: [
+                      DropdownMenuItem(
+                        child: Text("Seconds"),
+                        value: "Seconds",
+                      ),
+                      DropdownMenuItem(
+                        child: Text("Minutes"),
+                        value: "Minutes",
+                      ),
+                      DropdownMenuItem(
+                        child: Text("Hour"),
+                        value: "Hour",
+                      ),
+                    ],
+                    hint: Text(
+                      "Select Your Field.",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 10,
+                      ),
+                    ),
+                    onChanged: (_val) {
+                      setState(() {
+                        _selectedParam = _val;
+                      });
+                    },
+                  ),
+                  DropdownButton(
+                    value: val,
+                    items: [
+                      DropdownMenuItem(
+                        child: Text("1"),
+                        value: 1,
+                      ),
+                      DropdownMenuItem(
+                        child: Text("2"),
+                        value: 2,
+                      ),
+                      DropdownMenuItem(
+                        child: Text("3"),
+                        value: 3,
+                      ),
+                      DropdownMenuItem(
+                        child: Text("4"),
+                        value: 4,
+                      ),
+                    ],
+                    hint: Text(
+                      "Select Value",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 10
+                      ),
+                    ),
+                    onChanged: (_val) {
+                      setState(() {
+                        val = _val;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              RaisedButton(
+                onPressed: _showNotification,
+                child: new Text('Set Task With Notification'),
               ),
             ],
           ),
